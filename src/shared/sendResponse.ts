b41e4d87bd953c0dddc8544f542845386982fc3a
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-01 14:01:59 +0600

    Send Response func ADDED

---
+++ src/shared/sendResponse.ts
@@ -0,0 +1,18 @@
+import { Response } from "express";
+import { ApiResponse } from "@/types";
+
+const sendResponse = <T>(
+  res: Response,
+  { status, data, message, meta }: Omit<ApiResponse<T>, "error">
+): void => {
+  const payload: ApiResponse<T> = {
+    status,
+    error: null,
+    message,
+    data,
+    meta,
+  };
+  res.status(status).json(payload);
+};
+
+export default sendResponse;
