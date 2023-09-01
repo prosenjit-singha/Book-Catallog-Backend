a88c6657b0b7a63d5501291bd0a173575eae30cc
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-01 20:32:42 +0600

    module resolution DONE

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import "module-alias/register";
-import "tsconfig-paths/register";
+// Register alias always define it on top of module
+import moduleAlias from "module-alias";
+moduleAlias.addAlias("@", __dirname);
 
 import config from "@/config";
 
+console.log(__dirname);
 console.log(config);
