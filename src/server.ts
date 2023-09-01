283d0509527d896eb5b9c79cc6e091546030ae3b
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-01 20:45:28 +0600

    Server code ADDED

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,46 @@
 import moduleAlias from "module-alias";
 moduleAlias.addAlias("@", __dirname);
 
+import app from "./app";
+import { Server } from "http";
 import config from "@/config";
+// import { connectDB } from "@/helpers";
 
-console.log(__dirname);
-console.log(config);
+process.on("uncaughtException", (error) => {
+  console.log("UncaughtException", error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function main() {
+  try {
+    // await connectDB();
+    // console.log(`🛢 Database is connected successfully!`);
+
+    server = app.listen(config.port, () => {
+      console.log(`⚡Application listening on port - ${config.port}`);
+    });
+  } catch (err) {
+    console.log("Failed to connect database", err);
+  }
+
+  process.on("unhandledRejection", (error) => {
+    if (server) {
+      server.close(() => {
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+main();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM is received");
+  if (server) {
+    server.close();
+  }
+});
