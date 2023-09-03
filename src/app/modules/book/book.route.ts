4def9133ebdd0150f3991b7dd9eb19b58e68c5ce
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-03 20:40:26 +0600

    Book Route ADDED

---
+++ src/app/modules/book/book.route.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import * as ctrl from "./book.controller";
+import auth from "@/app/middlewares/auth.middleware";
+import validateReq from "@/app/middlewares/validateRequest";
+import * as schema from "./book.validation";
+
+const router = express.Router();
+
+router.get("/", auth("admin", "customer"), ctrl.getAllBooks);
+
+router.post(
+  "/",
+  validateReq(schema.createBook),
+  auth("admin", "customer"),
+  ctrl.createBook
+);
+
+router.get("/:id", auth("admin", "customer"), ctrl.getSingleBook);
+
+router.patch(
+  "/:id",
+  validateReq(schema.updateBook),
+  auth("admin", "customer"),
+  ctrl.updateBook
+);
+
+router.delete("/:id", auth("admin", "customer"), ctrl.deleteBook);
+
+export default router;
--- src/app/router.ts
+++ src/app/router.ts
@@ -3,6 +3,7 @@ import type { Router } from "express";
 import AuthRoute from "@/app/modules/auth/auth.route";
 import UserRoute from "@/app/modules/user/user.route";
 import CategoryRoute from "@/app/modules/book/category/category.route";
+import BookRoute from "@/app/modules/book/book.route";
 
 const router = express.Router();
 
@@ -23,6 +24,10 @@ const moduleRoutes: ModuleRoutes = [
   {
     path: "/categories",
     route: CategoryRoute,
+  },
+  {
+    path: "/categories",
+    route: BookRoute,
   },
 ];
 
