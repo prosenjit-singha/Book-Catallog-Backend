a620899352760c41bda7e718b4e49850af45e6a2
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-04 12:49:51 +0600

    update user validation added

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,13 +3,15 @@ import { z } from "zod";
 const role: UserRole[] = ["admin", "customer"];
 
 export const updateUserSchema = z.object({
-  body: z.object({
-    name: z.string().trim(),
-    email: z.string().email().trim(),
-    password: z.string(),
-    role: z.enum(role as [string, ...string[]]),
-    contactNo: z.string().trim(),
-    address: z.string().trim(),
-    profileImg: z.string().trim(),
-  }),
+  body: z
+    .object({
+      name: z.string().trim(),
+      email: z.string().email().trim(),
+      password: z.string(),
+      role: z.enum(role as [string, ...string[]]),
+      contactNo: z.string().trim(),
+      address: z.string().trim(),
+      profileImg: z.string().trim(),
+    })
+    .partial(),
 });
