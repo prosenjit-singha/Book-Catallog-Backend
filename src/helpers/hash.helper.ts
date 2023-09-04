ec53e22af0de012363f78e9f4285662d118b2267
prosenjit-singha <prosenjitsingha28@gmail.com>
2023-09-04 11:34:41 +0600

    bcrypt bug fixed

--- src/helpers/hash.helper.ts
+++ src/helpers/hash.helper.ts
@@ -2,4 +2,4 @@ import bcrypt from "bcrypt";
 import config from "@/config";
 
 const hashData = async (data: string) =>
-  await bcrypt.hash(data, config.bcrypt_salt_round as string);
+  await bcrypt.hash(data, config.bcrypt_salt_round as number);
